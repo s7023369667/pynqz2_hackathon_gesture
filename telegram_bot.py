6bf231074e8f69850a120d896c35725e2a8e9e53
Cheng Po Sheng <50870684+s7023369667@users.noreply.github.com>
2022-03-19 20:54:00 +0800

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,24 +1,25 @@
 import time,telegram
-
+CHAT_ID = **********
+TOKEN = **********
 class telegrambot():
     def __init__(self):
-        token = '1950280286:AAHkmBj7qW9zZ6A_9D3pwYlD5M3XT_Fuqfs'
+        token = TOKEN
         self.bot = telegram.Bot(token=token)
 
     def send_message(self,message):
-        mes = self.bot.send_message(chat_id=1963689302,text=message)
+        mes = self.bot.send_message(chat_id=CHAT_ID,text=message)
         return mes.message_id
 
     def send_photo(self,res):
         imgur = {7:'https://imgur.com/5KtIeQ2',8:'https://imgur.com/Ysrw1cF',
                  9:'https://imgur.com/prMcssG',10:'https://imgur.com/DAnNdDj',
                 11:'https://imgur.com/Q91py1u'}
-        photo = self.bot.send_photo(chat_id=1963689302,photo=imgur[res])
+        photo = self.bot.send_photo(chat_id=CHAT_ID,photo=imgur[res])
         self.del_message(photo.message_id)
 
     def del_message(self,del_id):
         time.sleep(2)
-        self.bot.delete_message(chat_id=1963689302, message_id=del_id)
+        self.bot.delete_message(chat_id=CHAT_ID, message_id=del_id)
 
 if __name__ == '__main__':
     tb = telegrambot()
